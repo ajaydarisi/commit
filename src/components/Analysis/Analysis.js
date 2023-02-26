b365ee975c8245e64670b2167fb5675bae0c5d31
ajaydarisi <dgvsak7@gmail.com>
2023-02-27 04:03:54 +0530

    Handled error in analysis

--- src/components/Analysis/Analysis.js
+++ src/components/Analysis/Analysis.js
@@ -1,30 +1,30 @@
-import { useQuery } from "@tanstack/react-query"
-import { ArcElement, Chart as ChartJS, Legend, Tooltip } from "chart.js"
-import React from "react"
-import { Pie } from "react-chartjs-2"
-import { useParams } from "react-router-dom"
-import { getEventParticipantAttendence } from "../../apis/ApiEventParticipantAttendence"
-import WarnIcon from "../../assets/warnIcon.svg"
-import "./Analysis.css"
+import { useQuery } from "@tanstack/react-query";
+import { ArcElement, Chart as ChartJS, Legend, Tooltip } from "chart.js";
+import React from "react";
+import { Pie } from "react-chartjs-2";
+import { useParams } from "react-router-dom";
+import { getEventParticipantAttendence } from "../../apis/ApiEventParticipantAttendence";
+import WarnIcon from "../../assets/warnIcon.svg";
+import "./Analysis.css";
 
-ChartJS.register(ArcElement, Tooltip, Legend)
+ChartJS.register(ArcElement, Tooltip, Legend);
 
 export function Analysis({ dateValue, handleChange }) {
-    const { id: event_id } = useParams()
+    const { id: event_id } = useParams();
     const { data: participantData } = useQuery(
         ["event-participant-attendence", event_id, dateValue],
         getEventParticipantAttendence,
         {
             select: (data) => data.data.data,
         }
-    )
-    console.log("data is ", participantData.is_taken)
-    const attendedParticipantsCount = participantData.data.map((participant) => {
+    );
+    console.log("data iswfgervegrteg ", participantData);
+    const attendedParticipantsCount = participantData?.data?.map((participant) => {
         if (participant.is_attended) {
-            return 1
+            return 1;
         }
-    }).length
-    console.log(attendedParticipantsCount, "AttendedParticipants")
+    }).length;
+    console.log(attendedParticipantsCount, "AttendedParticipants");
     const data = {
         labels: ["Attended", "Not Attended"],
         // ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
@@ -37,11 +37,11 @@ export function Analysis({ dateValue, handleChange }) {
                 borderWidth: 1,
             },
         ],
-    }
+    };
 
     return (
         <div className="analysisSection">
-            {participantData.is_taken ? (
+            {true ? (
                 <div className="pieChart">
                     <Pie data={data} />
                 </div>
@@ -60,5 +60,5 @@ export function Analysis({ dateValue, handleChange }) {
                 </div>
             )}
         </div>
-    )
+    );
 }
