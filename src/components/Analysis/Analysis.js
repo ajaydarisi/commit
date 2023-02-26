4e41714fa2f279973998f9f72d220cab4ab96a35
kalikayiCode <sunilkalikayiofficial@gmail.com>
2023-02-27 03:06:04 +0530

    warning model is styled

--- src/components/Analysis/Analysis.js
+++ src/components/Analysis/Analysis.js
@@ -1,13 +1,13 @@
-import { Button } from "@mui/material";
-import { useQuery } from "@tanstack/react-query";
-import { ArcElement, Chart as ChartJS, Legend, Tooltip } from "chart.js";
-import React from "react";
-import { Pie } from "react-chartjs-2";
-import { useParams } from "react-router-dom";
-import { getEventParticipantAttendence } from "../../apis/ApiEventParticipantAttendence";
-import "./Analysis.css";
+import { useQuery } from "@tanstack/react-query"
+import { ArcElement, Chart as ChartJS, Legend, Tooltip } from "chart.js"
+import React from "react"
+import { Pie } from "react-chartjs-2"
+import { useParams } from "react-router-dom"
+import { getEventParticipantAttendence } from "../../apis/ApiEventParticipantAttendence"
+import WarnIcon from "../../assets/warnIcon.svg"
+import "./Analysis.css"
 
-ChartJS.register(ArcElement, Tooltip, Legend);
+ChartJS.register(ArcElement, Tooltip, Legend)
 
 export const data = {
     labels: ["Attended", "Not Attended"],
@@ -21,27 +21,35 @@ export const data = {
             borderWidth: 1,
         },
     ],
-};
+}
 
 export function Analysis({ dateValue, handleChange }) {
-    const { id: event_id } = useParams();
+    const { id: event_id } = useParams()
     const { data } = useQuery(["event-participant-attendence", event_id, dateValue], getEventParticipantAttendence, {
         select: (data) => data.data.data,
-    });
-    console.log("data is ", data.is_taken);
+    })
+    console.log("data is ", data.is_taken)
 
     return (
-        <div className="pieChart">
+        <div className="analysisSection">
             {data.is_taken ? (
-                <Pie data={data} />
+                <div className="pieChart">
+                    <Pie data={data} />
+                </div>
             ) : (
-                <div>
-                    <div>Attendance not taken</div>
-                    <Button variant="contained" onClick={() => handleChange(0, 0)}>
+                <div className="warningSection">
+                    <img src={WarnIcon} alt="" className="warnIcon" />
+                    <div style={{ marginBottom: "10px" }}>Attendance not taken ...!</div>
+                    <button
+                        variant="contained"
+                        onClick={() => handleChange(0, 0)}
+                        className="PrimaryBtn"
+                        style={{ height: "35px" }}
+                    >
                         Go to sadhaks
-                    </Button>
+                    </button>
                 </div>
             )}
         </div>
-    );
+    )
 }
