5814fe7469856ef8524b29c5cf05a0fdd71d1563
kalikayiCode <sunilkalikayiofficial@gmail.com>
2023-02-26 17:21:34 +0530

    merged

--- src/components/Dashboard/Dashboard.js
+++ src/components/Dashboard/Dashboard.js
@@ -1,16 +1,14 @@
-import { useQuery } from "@tanstack/react-query";
-import React from "react";
-import { getEventsByGroupId } from "../../apis/ApiEvent";
-import EventCard from "../EventCard/EventCard";
-import Header from "./Header/Header";
-import Navbar from "./Navbar/Navbar";
+import React from "react"
+import EventSection from "../EventSection/EventSection"
+import "./Dashboard.css"
+import Navbar from "./Navbar/Navbar"
 
 function Dashboard() {
     const cardData = {
         title: "Event title",
         time: "7:00 am - 8:00 am",
         attended: 20,
-    };
+    }
     return (
         <div className="app">
             <div className="navbar">
@@ -20,7 +18,7 @@ function Dashboard() {
                 <EventSection />
             </div>
         </div>
-    );
+    )
 }
 
-export default Dashboard;
+export default Dashboard
