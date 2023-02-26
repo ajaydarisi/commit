4c6bd62f7ad6d4bd0513dc08b406e20c1d97e440
bala nagaraju <payyavula@payatu.io>
2023-02-26 16:30:15 +0530

    fiexed

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter as Router } from "react-router-dom";
@@ -15,8 +16,8 @@ root.render(
             <Router>
                 <App />
             </Router>
+            <ReactQueryDevtools />
         </QueryClientProvider>
-        {/* <ReactQueryDevtools /> */}
     </div>
 );
 
