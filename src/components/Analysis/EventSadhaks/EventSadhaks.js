d7334161142c059f84f1d3cdb07e45dd7a644697
kalikayiCode <sunilkalikayiofficial@gmail.com>
2023-02-27 01:39:49 +0530

    check box and input field is added

--- src/components/Analysis/EventSadhaks/EventSadhaks.js
+++ src/components/Analysis/EventSadhaks/EventSadhaks.js
@@ -1,54 +1,58 @@
-import { TextField } from "@mui/material";
-import { DataGrid } from "@mui/x-data-grid";
-import { useQuery } from "@tanstack/react-query";
-import { getParticipantsByGroupId } from "../../../apis/ApiParticipant";
+import { TextField } from "@mui/material"
+import Checkbox from "@mui/material/Checkbox"
+import { DataGrid } from "@mui/x-data-grid"
+import { useQuery } from "@tanstack/react-query"
+import React from "react"
+import { getParticipantsByGroupId } from "../../../apis/ApiParticipant"
 
 const RenderAttendance = () => {
-    return <TextField id="standard-basic" label="Standard" variant="standard" />;
-};
+    return <TextField id="standard-basic" label="Standard" variant="standard" />
+}
 
 const EventSadhaks = () => {
     const { data = [] } = useQuery(["participants"], getParticipantsByGroupId, {
         select: (data) => data.data.data,
-    });
-    console.log("sadhaks data is", data);
+    })
+    const label = { inputProps: { "aria-label": "Checkbox demo" } }
+    const columns = [
+        {
+            field: "id",
+            headerName: "S.no",
+            width: 100,
+        },
+        {
+            field: "name",
+            headerName: "name",
+            width: 300,
+        },
+        {
+            field: "mobile",
+            headerName: "mobile number",
+            width: 300,
+        },
+        {
+            field: "is_attended",
+            headerName: "Is Attended",
+            width: 300,
+            renderCell: () => {
+                return <Checkbox {...label} />
+            },
+        },
+        {
+            field: "reason",
+            headerName: "Reason",
+            width: 300,
+            renderCell: () => {
+                return <TextField id="outlined-basic" label="Reason" variant="outlined" />
+            },
+        },
+    ]
+
+    console.log("sadhaks data is", data)
     return (
         <div style={{ height: 300, width: "100%" }}>
             <DataGrid rows={data} columns={columns} hideFooter autoHeight />
         </div>
-    );
-};
-export default EventSadhaks;
-
-const columns = [
-    {
-        field: "id",
-        headerName: "S.no",
-        width: 100,
-    },
-    {
-        field: "name",
-        headerName: "name",
-        width: 300,
-    },
-    {
-        field: "mobile",
-        headerName: "mobile number",
-        width: 300,
-    },
-    {
-        field: "is_attended",
-        headerName: "Is Attended",
-        width: 300,
-        renderCell: ()=>{
-            return(
-                <div>helllooo</div>
-            )
-        },
-    },
-    {
-        field: "reason",
-        headerName: "Reason",
-        width: 300,
-    },
-];
+    )
+}
+export default EventSadhaks
--- src/components/EventBody/EventBody.js
+++ src/components/EventBody/EventBody.js
@@ -1,14 +1,14 @@
-import Box from "@mui/material/Box";
-import Tab from "@mui/material/Tab";
-import Tabs from "@mui/material/Tabs";
-import Typography from "@mui/material/Typography";
-import PropTypes from "prop-types";
-import * as React from "react";
-import { Analysis } from "../Analysis/Analysis";
-import EventSadhaks from "../Analysis/EventSadhaks/EventSadhaks";
+import Box from "@mui/material/Box"
+import Tab from "@mui/material/Tab"
+import Tabs from "@mui/material/Tabs"
+import Typography from "@mui/material/Typography"
+import PropTypes from "prop-types"
+import React from "react"
+import { Analysis } from "../Analysis/Analysis"
+import EventSadhaks from "../Analysis/EventSadhaks/EventSadhaks"
 
 function TabPanel(props) {
-    const { children, value, index, ...other } = props;
+    const { children, value, index, ...other } = props
 
     return (
         <div
@@ -24,37 +24,33 @@ function TabPanel(props) {
                 </Box>
             )}
         </div>
-    );
+    )
 }
 
 TabPanel.propTypes = {
     children: PropTypes.node,
     index: PropTypes.number.isRequired,
     value: PropTypes.number.isRequired,
-};
+}
 
 function a11yProps(index) {
     return {
         id: `simple-tab-${index}`,
         "aria-controls": `simple-tabpanel-${index}`,
-    };
+    }
 }
 
 export default function BasicTabs() {
-    const [value, setValue] = React.useState(0);
+    const [value, setValue] = React.useState(0)
 
     const handleChange = (event, newValue) => {
-        setValue(newValue);
-    };
+        setValue(newValue)
+    }
 
     return (
         <Box sx={{ width: "100%" }}>
             <Box sx={{ borderBottom: 1, borderColor: "divider" }}>
-                <Tabs
-                    value={value}
-                    onChange={handleChange}
-                    aria-label="basic tabs example"
-                >
+                <Tabs value={value} onChange={handleChange} aria-label="basic tabs example">
                     <Tab label="Sadhaks" {...a11yProps(0)} />
                     <Tab label="Analysis" {...a11yProps(1)} />
                 </Tabs>
@@ -66,5 +62,5 @@ export default function BasicTabs() {
                 <Analysis />
             </TabPanel>
         </Box>
-    );
+    )
 }
