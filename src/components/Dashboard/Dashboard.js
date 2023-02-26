4c14d80e87fb3049babae532809eac898f30e9ea
kalikayiCode <sunilkalikayiofficial@gmail.com>
2023-02-26 16:43:55 +0530

    event body section is creted

--- src/components/Dashboard/Dashboard.js
+++ src/components/Dashboard/Dashboard.js
@@ -1,16 +1,14 @@
 import React from "react"
+import EventSection from "../EventSection/EventSection"
 import "./Dashboard.css"
+import Navbar from "./Navbar/Navbar"
 
 function Dashboard() {
-    // const cardData = {
-    //     title: "Event title",
-    //     time: "7:00 am - 8:00 am",
-    //     attended: 20,
-    // }
+   
     return (
         <div className="app">
-            <div className="navbar"><Navb/> </div>
-            <div className="EventSection">djfoiajdflkjdl</div>
+            <Navbar />
+            <EventSection />
         </div>
     )
 }
--- src/components/EventCard/EventCard.js
+++ src/components/EventCard/EventCard.js
@@ -1,13 +1,12 @@
 import KeyboardArrowRightIcon from "@mui/icons-material/KeyboardArrowRight"
 import Button from "@mui/material/Button"
 import React from "react"
-import ImageOne from "../../assets/SudarshanKriya.jpg"
 import "./EventCard.css"
 
 function EventCard({ cardData }) {
     return (
         <div className="card">
-            <img src={ImageOne} alt="evet Image" />
+            <img src={cardData.image} alt="evet Image" />
             <div className="cardContent">
                 <div className="title">{cardData.title}</div>
                 <div className="textMedium">{cardData.time}</div>
