3a5f7c4f82d02a06872a4e5d4dcaf582839116cf
ajaydarisi <dgvsak7@gmail.com>
2023-02-26 18:19:45 +0530

    Date picker

--- src/components/Event/Event.js
+++ src/components/Event/Event.js
@@ -1,8 +1,29 @@
-import React from "react"
-import PageLayout from "../PageLayout/PageLayout"
+import TextField from "@mui/material/TextField";
+import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
+import { DesktopDatePicker } from "@mui/x-date-pickers/DesktopDatePicker";
+import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
+import dayjs from "dayjs";
+import React, { useState } from "react";
+import PageLayout from '../PageLayout/PageLayout'
 
-function Event() {
-    return <PageLayout>paste your component heree......!</PageLayout>
+export default function MaterialUIPickers() {
+    const [value, setValue] = useState(dayjs("2014-08-18T21:11:54"));
+
+    const handleChange = (newValue) => {
+        setValue(newValue);
+    };
+
+    return (
+        <PageLayout>
+            <LocalizationProvider dateAdapter={AdapterDayjs}>
+                <DesktopDatePicker
+                    label="Date"
+                    inputFormat="MM/DD/YYYY"
+                    value={value}
+                    onChange={handleChange}
+                    renderInput={(params) => <TextField {...params} />}
+                />
+            </LocalizationProvider>
+        </PageLayout>
+    );
 }
-
-export default Event
