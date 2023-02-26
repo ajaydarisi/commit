5bfb0fd6b8713a7b9acd6e7f24ca30d632cb713e
bala nagaraju <payyavula@payatu.io>
2023-02-26 17:25:45 +0530

    taking pull

--- src/pages/sadhaks/SadhaksPage.js
+++ src/pages/sadhaks/SadhaksPage.js
@@ -1,13 +1,16 @@
 import { useQuery } from "@tanstack/react-query";
 import React from "react";
 import { getParticipantsByGroupId } from "../../apis/ApiParticipant";
+import Navbar from "../../components/Dashboard/Navbar/Navbar";
 
 function SadhaksPage() {
     const { data = [] } = useQuery(["participants"], getParticipantsByGroupId, {
         select: data => data.data.data,
     });
     console.log("sadhaks data is", data);
-    return <div>SadhaksPage</div>;
+    return <div>
+      <Navbar />
+    </div>;
 }
 
 export default SadhaksPage;
