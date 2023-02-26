1ab251ed1c407755eae1c1067068c5bff892f905
bala nagaraju <payyavula@payatu.io>
2023-02-26 17:05:00 +0530

    commented event qyery code

--- src/components/Dashboard/Dashboard.js
+++ src/components/Dashboard/Dashboard.js
@@ -18,3 +18,32 @@ function Dashboard() {
 }
 
 export default Dashboard
+
+
+// import { useQuery } from "@tanstack/react-query";
+// import React from "react";
+// import { getEventsByGroupId } from "../../apis/ApiEvent";
+// import EventCard from "../EventCard/EventCard";
+// import Header from "./Header/Header";
+// import Navbar from "./Navbar/Navbar";
+
+// function Dashboard() {
+//     const cardData = {
+//         title: "Event title",
+//         time: "7:00 am - 8:00 am",
+//         attended: 20,
+//     };
+//     const { data =[]} = useQuery(["events"], getEventsByGroupId, {
+//         select: data => data.data.data,
+//     });
+//     console.log("data is ", data);
+//     return (
+//         <div>
+//             <Navbar />
+//             <Header />
+//             <EventCard cardData={cardData} />
+//         </div>
+//     );
+// }
+
+// export default Dashboard;
