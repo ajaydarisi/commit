8c5fb825d9c05c748c46bf41c6c6643c5f84ef47
ajaydarisi <dgvsak7@gmail.com>
2023-02-26 16:43:25 +0530

    Working on header

--- src/components/Dashboard/Navbar/Navbar.js
+++ src/components/Dashboard/Navbar/Navbar.js
@@ -1,19 +1,20 @@
 import { Typography } from "@mui/material";
 import { Box } from "@mui/system";
 import React from "react";
+import './Navbar.css'
 
 function Navbar() {
     return (
-        <div>
+        <div className="navbarMain">
             <Box
                 sx={{
                     backgroundColor: "#F8F5EF",
-                    height: "40px",
+                    height: "60px",
                     width: "100%",
-                    padding: "10px"
+                    padding: "10px",
                 }}
             >
-                <Typography variant="h4" component="h1" color={"#59595B"}>
+                <Typography variant="h4" color={"#59595B"}>
                     Commit
                 </Typography>
             </Box>
