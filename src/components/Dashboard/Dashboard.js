7fe44d3f437fc7303b7fc8d3e72b435ad43e8554
ajaydarisi <dgvsak7@gmail.com>
2023-02-26 17:13:55 +0530

    Working on events API integration

--- src/components/Dashboard/Dashboard.js
+++ src/components/Dashboard/Dashboard.js
@@ -1,7 +1,7 @@
-import React from "react"
-import EventSection from "../EventSection/EventSection"
-import "./Dashboard.css"
-import Navbar from "./Navbar/Navbar"
+import React from "react";
+import EventSection from "../EventSection/EventSection";
+import "./Dashboard.css";
+import Navbar from "./Navbar/Navbar";
 
 function Dashboard() {
     return (
@@ -14,36 +14,7 @@ function Dashboard() {
                 <EventSection />
             </div>
         </div>
-    )
+    );
 }
 
-export default Dashboard
-
-
-// import { useQuery } from "@tanstack/react-query";
-// import React from "react";
-// import { getEventsByGroupId } from "../../apis/ApiEvent";
-// import EventCard from "../EventCard/EventCard";
-// import Header from "./Header/Header";
-// import Navbar from "./Navbar/Navbar";
-
-// function Dashboard() {
-//     const cardData = {
-//         title: "Event title",
-//         time: "7:00 am - 8:00 am",
-//         attended: 20,
-//     };
-//     const { data =[]} = useQuery(["events"], getEventsByGroupId, {
-//         select: data => data.data.data,
-//     });
-//     console.log("data is ", data);
-//     return (
-//         <div>
-//             <Navbar />
-//             <Header />
-//             <EventCard cardData={cardData} />
-//         </div>
-//     );
-// }
-
-// export default Dashboard;
+export default Dashboard;
